749c75075316783681da1852e178084e9c86742b
Cuong Tran <67938422+cuongtnh@users.noreply.github.com>
2020-07-07 08:39:54 +0100

    Update create-vm-script.ps1

--- create-vm-script.ps1
+++ create-vm-script.ps1
@@ -1,15 +1,14 @@
-$resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
-$location = Read-Host -Prompt "Enter the location (i.e. centralus)"
+$resourceGroupName = Get-AzResource | Select ResourceGroupName
+$location = Get-AzResource | Select Location
 $adminUsername = Read-Host -Prompt "Enter the administrator username"
 $adminPassword = Read-Host -Prompt "Enter the administrator password" -AsSecureString
 $dnsLabelPrefix = Read-Host -Prompt "Enter an unique DNS name for the public IP"
 
-New-AzResourceGroup -Name $resourceGroupName -Location "$location"
+#New-AzResourceGroup -Name $resourceGroupName -Location "$location"
 New-AzResourceGroupDeployment `
     -ResourceGroupName $resourceGroupName `
-    -TemplateUri "https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-vm-simple-windows/azuredeploy.json" `
+    -TemplateUri "https://raw.githubusercontent.com/cuongtnh/Azure-Script/master/DC01-template.json" `
     -adminUsername $adminUsername `
     -adminPassword $adminPassword `
-    -dnsLabelPrefix $dnsLabelPrefix
 
  (Get-AzVm -ResourceGroupName $resourceGroupName).name
